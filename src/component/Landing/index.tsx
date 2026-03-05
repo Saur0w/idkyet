af735b7dc213d6fe3d95c90d96302cb339beddbf
saur0w <sthapliyal085@gmail.com>
2026-03-05 23:12:41 +0530

    "Setup"

---
+++ src/component/Landing/index.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import styles from "./style.module.scss";
+
+export default function Landing() {
+    return (
+        <div className={styles.landing}>
+
+        </div>
+    )
+}
